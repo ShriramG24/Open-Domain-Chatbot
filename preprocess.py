32f52330810a61f2294b7624c7f70e8d490ce83e
ShriramG24 <shrirambhavin@gmail.com>
2022-06-07 13:43:06 -0400

    Switch branches

--- preprocess.py
+++ preprocess.py
@@ -6,7 +6,8 @@ class PreprocessData:
     def clean_data(path):
         with open(path, 'r') as file:
             df = pd.read_json(file)
-            df.drop(df[(df.eval_score is None) or (df.profile_match == "")].index, inplace=True)
+        
+        df.drop(df[(df.eval_score is None) or (df.profile_match == "")].index, inplace=True)
             
 
         df.to_json('ConvAI2_DS/new_dialogues.json', orient='records')
