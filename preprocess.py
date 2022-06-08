1ddb8003ebb8855a88fa31b5c6b9c880acbb9a78
ShriramG24 <shrirambhavin@gmail.com>
2022-06-08 13:38:15 -0400

    Added preprocess utils

--- preprocess.py
+++ preprocess.py
@@ -1,18 +1,21 @@
-import pandas as pd
+import json
 import nltk
 from nltk.stem import LancasterStemmer
 
-class PreprocessData:
-    def clean_data(self, path):
-        with open(path, 'r') as file:
-            df = pd.read_json(file)
-        
-        
-        
-        df.to_json('new_dialogues.json', orient='records')
+class NLPPreprocess:
+    def __init__(self, stemmer):
+        self.stemmer = stemmer
 
-dataset = PreprocessData()
-dataset.clean_data('dialogues.json')
+    def tokenize(self, dialogue):
+        return nltk.word_tokenize(dialogue)
+        
+    def stem(self, word):
+        return self.stemmer.stem(word.lower())
+
+    def bag_of_words(sentence, word_bank):
+        pass
+
+
 
 
 
