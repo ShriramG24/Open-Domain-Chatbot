900fac4a1ed3db912f4df44f7e9be0c30cb4b6a9
ShriramG24 <shrirambhavin@gmail.com>
2022-06-08 09:33:39 -0400

    Switch dataset

--- preprocess.py
+++ preprocess.py
@@ -3,16 +3,16 @@ import nltk
 from nltk.stem import LancasterStemmer
 
 class PreprocessData:
-    def clean_data(path):
+    def clean_data(self, path):
         with open(path, 'r') as file:
             df = pd.read_json(file)
         
-        df.drop(df[(df.eval_score is None) or (df.profile_match == "")].index, inplace=True)
-    
+        
+        
+        df.to_json('new_dialogues.json', orient='records')
 
-        df.to_json('ConvAI2_DS/new_dialogues.json', orient='records')
-
-
+dataset = PreprocessData()
+dataset.clean_data('dialogues.json')
 
 
 
